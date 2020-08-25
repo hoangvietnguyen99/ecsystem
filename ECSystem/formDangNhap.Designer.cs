1242f19317c7e065a2c9a98cab8e5d2fc6670741
hoangvietnguyen99 <hoangvietnguyen99@gmail.com>
2020-08-25 19:19:34 +0700

    init commit

---
+++ ECSystem/formDangNhap.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace ECSystem
+{
+    partial class formDangNhap
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBoxUsername = new MaterialSkin.Controls.MaterialTextBox();
+            this.textBoxPassword = new MaterialSkin.Controls.MaterialTextBox();
+            this.buttonExit = new MaterialSkin.Controls.MaterialButton();
+            this.buttonLogin = new MaterialSkin.Controls.MaterialButton();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.buttonLogin);
+            this.panel1.Controls.Add(this.buttonExit);
+            this.panel1.Controls.Add(this.textBoxPassword);
+            this.panel1.Controls.Add(this.textBoxUsername);
+            this.panel1.Location = new System.Drawing.Point(12, 72);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(376, 165);
+            this.panel1.TabIndex = 0;
+            // 
+            // textBoxUsername
+            // 
+            this.textBoxUsername.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxUsername.Depth = 0;
+            this.textBoxUsername.Font = new System.Drawing.Font("Roboto", 12F);
+            this.textBoxUsername.Hint = "Username";
+            this.textBoxUsername.Location = new System.Drawing.Point(3, 3);
+            this.textBoxUsername.MaxLength = 50;
+            this.textBoxUsername.MouseState = MaterialSkin.MouseState.OUT;
+            this.textBoxUsername.Multiline = false;
+            this.textBoxUsername.Name = "textBoxUsername";
+            this.textBoxUsername.Size = new System.Drawing.Size(370, 50);
+            this.textBoxUsername.TabIndex = 0;
+            this.textBoxUsername.Text = "";
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxPassword.Depth = 0;
+            this.textBoxPassword.Font = new System.Drawing.Font("Roboto", 12F);
+            this.textBoxPassword.Hint = "Password";
+            this.textBoxPassword.Location = new System.Drawing.Point(3, 59);
+            this.textBoxPassword.MaxLength = 50;
+            this.textBoxPassword.MouseState = MaterialSkin.MouseState.OUT;
+            this.textBoxPassword.Multiline = false;
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Password = true;
+            this.textBoxPassword.Size = new System.Drawing.Size(370, 50);
+            this.textBoxPassword.TabIndex = 1;
+            this.textBoxPassword.Text = "";
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.buttonExit.Depth = 0;
+            this.buttonExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.buttonExit.DrawShadows = true;
+            this.buttonExit.HighEmphasis = true;
+            this.buttonExit.Icon = null;
+            this.buttonExit.Location = new System.Drawing.Point(320, 118);
+            this.buttonExit.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.buttonExit.MouseState = MaterialSkin.MouseState.HOVER;
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(52, 36);
+            this.buttonExit.TabIndex = 3;
+            this.buttonExit.Text = "Exit";
+            this.buttonExit.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
+            this.buttonExit.UseAccentColor = false;
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // buttonLogin
+            // 
+            this.buttonLogin.AutoSize = false;
+            this.buttonLogin.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.buttonLogin.Depth = 0;
+            this.buttonLogin.DrawShadows = true;
+            this.buttonLogin.HighEmphasis = true;
+            this.buttonLogin.Icon = null;
+            this.buttonLogin.Location = new System.Drawing.Point(194, 118);
+            this.buttonLogin.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.buttonLogin.MouseState = MaterialSkin.MouseState.HOVER;
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(118, 36);
+            this.buttonLogin.TabIndex = 2;
+            this.buttonLogin.Text = "Login";
+            this.buttonLogin.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
+            this.buttonLogin.UseAccentColor = false;
+            this.buttonLogin.UseVisualStyleBackColor = true;
+            this.buttonLogin.Click += new System.EventHandler(this.buttonLogin_Click);
+            // 
+            // formDangNhap
+            // 
+            this.AcceptButton = this.buttonLogin;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.buttonExit;
+            this.ClientSize = new System.Drawing.Size(400, 249);
+            this.Controls.Add(this.panel1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "formDangNhap";
+            this.Sizable = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Đăng nhập";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.formDangNhap_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private MaterialSkin.Controls.MaterialButton buttonLogin;
+        private MaterialSkin.Controls.MaterialButton buttonExit;
+        private MaterialSkin.Controls.MaterialTextBox textBoxPassword;
+        private MaterialSkin.Controls.MaterialTextBox textBoxUsername;
+    }
+}
---
+++ ECSystem/formDangNhap.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using MaterialSkin;
+using MaterialSkin.Controls;
+
+namespace ECSystem
+{
+    public partial class formDangNhap : MaterialForm
+    {
+        public formDangNhap()
+        {
+            InitializeComponent();
+
+            var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+            materialSkinManager.ColorScheme = new ColorScheme(
+                Primary.BlueGrey800, Primary.BlueGrey900, Primary.BlueGrey500, Accent.LightBlue200, TextShade.WHITE);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void formDangNhap_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("Are you sure to exit?", "Alert", MessageBoxButtons.OKCancel) != DialogResult.OK)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+            formQuanLy form = new formQuanLy();
+            this.Hide();
+            form.ShowDialog();
+            this.Show();
+        }
+    }
+}
