c83dfa628ff540d65e9a4b0362310d21b80576a2
Viet <bachvietthuduc1999@gmail.com>
2020-08-25 19:30:01 +0700

    them class lHang, mHang

---
+++ ECSystem/DTO/LoaiHang.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECSystem.DTO
+{
+    class LoaiHang
+    {
+        public LoaiHang(string maLoaiHang, string tenLoaiHang, string nvQuanLy)
+        {
+           // this.MaLoaiHang = maLoaiHang;
+        }
+
+    }
+}
